1a6b571468e5e1c28f0bb161db61efaabdbfc60a
naviza <43558584+naviza@users.noreply.github.com>
2019-01-20 02:40:54 -0700

    clears the page then prints the information

--- oranges.js
+++ oranges.js
@@ -1,30 +1,104 @@
 
 var fade = 0;
 var page = 0;
+var initDimensions = (0,0);
+
+var dicc0 = {
+"00-10": 0.8,
+"10-20": 0.54,
+"20-30": 0.12,
+"30-40": 0.65,
+"40-50": 0.76,
+"50-60": 0.98,
+"60-70": 0.12,
+"70-80": 0.13
+};
+var dicc1 = {
+"00-10": 0.8,
+"10-20": 0.5,
+"20-30": 0.23,
+"30-40": 0.25,
+"70-80": 0.72,
+"80-90": 0.65,
+"90-100": 0.78
+};
+var dicc2 = {
+  "00-10": 0.8,
+  "10-20": 0.54,
+  "20-30": 0.12,
+  "30-40": 0.65,
+  "40-50": 0.76,
+  "50-60": 0.98,
+  "60-70": 0.12,
+  "70-80": 0.13,
+  "80-90": 0.23,
+  "90-100": 0.57
+};
+var dicc3 = {
+  "00-10": 0.8,
+  "10-20": 0.5,
+  "20-30": 0.23,
+  "30-40": 0.25,
+  "40-50": 0.90,
+  "50-60": 0.98,
+  "60-70": 0.13,
+  "70-80": 0.72,
+  "80-90": 0.65,
+  "90-100": 0.78
+};
+var diccList = [dicc0, dicc1, dicc2, dicc3];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   background('#a5c7ff');
   triangles();
+  initDimensions = (windowWidth, windowHeight);
 }
 
 function draw() {
-  if (start == true) {
-    for (i=1; i<255; i++){
-    fade = fade++;
-    fill(color(255, 255, 255, fade));
-    rect(0 , 0, windowWidth, windowHeight, 5, 5, 5, 5);
-  }
-  }
-  titleScreen();
-  begin();
-
   if (page) {
-    richGame(page);
+    console.log(page);
+    richGame(diccList[page]);
+  } if (page == 0) {
+    if (start == true) {
+      for (i=1; i<255; i++){
+      fade = fade++;
+      fill(color(255, 255, 255, fade));
+      rect(0 , 0, windowWidth, windowHeight, 5, 5, 5, 5);
+    }
+    }
+    titleScreen();
+    begin();
+  } else if (page >= diccList.length) {
+    endGame();
   }
 }
 
+function draw() {
+
+
+  if (page) {
+    console.log(page);
+    richGame(diccList[page]);
+  } else {
+    if (start == true) {
+      for (i=1; i<255; i++){
+      fade = fade++;
+      fill(color(255, 255, 255, fade));
+      rect(0 , 0, windowWidth, windowHeight, 5, 5, 5, 5);
+    }
+    }
+    titleScreen();
+    begin();
+  }
+}
+
+
 function mousePressed() {
   page++;
+  clearPage(initDimensions);
+  if (page >= diccList.length) {
+    endGame();
+  }
 }
--- richGame.js
+++ richGame.js
@@ -1,6 +1,12 @@
 // xCor, yCor
 // R G B opacity
 // fill(color(255, 255, 255, titleTransitionWhite));
+function printXlabel(xCor, yCor, key) {
+  console.log("we really here");
+  fill(0, 102, 153);
+  textSize(32);
+  text('word',xCor, yCor);
+}
 
 // colour is a string of the hex value of the colour with # infront
 // this function draws with shadows
@@ -14,50 +20,38 @@ function drawBox(R,G,B,xCor, fraction) {
   var yCor = baseY - height;
 
   // (x,y) coordinate ; // width and height
-
-  for (i=1; i<height;i++) {
-    fill((color(R-10,G-10,B-20)));
-    rect(xCor+3, yCor+3, 55, i, boxRadius);
-    fill((color(R,G,B)));
-    rect(xCor, yCor, 55, i, boxRadius);
-  }
+  fill((color(R-10,G-10,B-20)));
+  rect(xCor+3, yCor+3, 55, height, boxRadius);
+  fill((color(R,G,B)));
+  rect(xCor, yCor, 55, height, boxRadius);
+  printXlabel(xCor, baseY + 30, key)
 }
 
-function richGame(page) {
+function richGame(diccionario) {
   // console.log(page);
   var barHeight = 300;
   var barWidth = 50;
-  var barSpace = 50;
+  var barSpace = 30;
   var baseX = 50;
   var baseY = 500;
   var xCor = 90;
+  var firstBarSpace = 20;
 
-  var diccionario = {
-  "00-10": 0.8,
-  "10-20": 0.54,
-  "20-30": 0.12,
-  "30-40": 0.65,
-  "40-50": 0.76,
-  "50-60": 0.98,
-  "60-70": 0.12,
-  "70-80": 0.13,
-  "80-90": 0.23,
-  "90-100": 0.57
-};
   // returns a list of keys to the dictionary
   const keys = Object.keys(diccionario);
+
+  // iterate over the keys to print the values
   for (i=0;i<(keys.length);i++) {
     // console.log(keys[i]);
     xCor = baseX + i*(barSpace+barWidth);
-    drawBox(255,179,98,xCor,diccionario[keys[i]]);
+    drawBox(255,179,98,xCor + firstBarSpace,diccionario[keys[i]]);
   }
-  var xAxisLength = i*(barSpace+barWidth);
+  var xAxisLength = i*(barSpace+barWidth) + firstBarSpace;
 
   //print the box starting from the base to the top
   fill('#000000');
 
   // this is the horizontal line
-
   rect(baseX, baseY + 3,xAxisLength,3);
   var height =  30 + barHeight;
   var yCor = baseY - height + 6;
@@ -65,4 +59,16 @@ function richGame(page) {
   // this is the horizontal
   rect(baseX - 3, yCor,3,height);
 
+}
+
+function clearPage(initDimensions) {
+  console.log("We here.");
+  background('#ffffff');
+  drawBox(0,0,initDimensions[0], initDimensions[1]);
+}
+
+function endGame(initDimensions) {
+  console.log("End Game");
+  background('#000000');
+  drawBox(0,0,initDimensions[0], initDimensions[1]);
 }
