ce628b8f1a6bbf9c7a72aa9d09fc9a9d66288458
Lora <lora_ma@hotmail.ca>
2019-01-19 22:45:25 -0700

    i hate life less <:

--- oranges.js
+++ oranges.js
@@ -1,32 +1,10 @@
-var titleTransition = 0;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   background('#a5c7ff');
-//  fill('white');
-//  rect(25, 25, windowWidth - 50, windowHeight - 50, 5, 5, 5, 5);
-//  fill('#a5c7ff');
-//  rect(35, 35, windowWidth - 70, windowHeight - 70, 5, 5, 5, 5);
-  fill(color(5,5,5,15));
-  rect(370, 110, 800, 150, 5, 5, 5, 5);
-  fill('0, 200, 100, 15')
-  rect(360, 100, 800, 150, 5, 5, 5, 5);
-  fill('#a5c7ff');
-  textFont('monospace', 50);
-  textSize(50);
-  text('GitHub Statistics',530 , 150, 600, 100 );
-
-  fill(color(5,5,5,15));
-  rect(710, 285, 450 , 50, 5, 5, 5, 5);
-  fill('white');
-  rect(700, 275, 450 , 50, 5, 5, 5, 5);
-
-
 
 }
 
 function draw() {
-
-
+  titleScreen();
 }
---
+++ sketch.js
@@ -0,0 +1,31 @@
+var titleTransition = 0;
+var titleTransitionWhite = 100;
+var writing = 0
+
+function titleScreen(){
+  writing = writing + 8;
+  titleTransition=titleTransition+2;
+  titleTransitionWhite= titleTransitionWhite+2;
+
+  //Title box and screen
+  fill(color(162, 188, 229, titleTransition));
+  rect(370, 110, 800, 150, 5, 5, 5, 5);
+  fill(color(255, 255, 255, titleTransitionWhite));
+  rect(360, 100, 800, 150, 5, 5, 5, 5);
+  fill(color(165, 199, 255, writing));
+  textFont('monospace', 50);
+  textSize(50);
+  text('GitHub Statistics',530 , 150, 600, 100);
+
+  //Subtitle
+  fill(color(162, 188, 229, titleTransition));
+  rect(610, 275, 560 , 40, 5, 5, 5, 5);
+  fill(color(255, 255, 255, titleTransitionWhite));
+  rect(600, 265, 560 , 40, 5, 5, 5, 5);
+
+  fill(color(165, 199, 255, writing));
+  textFont('monospace', 50);
+  textSize(17);
+  text('Mac Malainey, Hudson Shykowski, Richmond Naviza, Lora Ma', 618 , 278, 600, 50);
+
+}
