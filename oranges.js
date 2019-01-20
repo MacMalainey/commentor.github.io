dd32b33daa623a3c9e7c65276da29c378fb954dc
Lora <lora_ma@hotmail.ca>
2019-01-20 01:09:57 -0700

    Yayy. website looks pretty!

--- oranges.js
+++ oranges.js
@@ -1,10 +1,21 @@
+var fade = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   background('#a5c7ff');
-
+  triangles();
 }
 
 function draw() {
+  if (start == true) {
+    for (i=1; i<255; i++){
+    fade = fade++;
+    fill(color(255, 255, 255, fade));
+    rect(0 , 0, windowWidth, windowHeight, 5, 5, 5, 5);
+  }
+  }
   titleScreen();
+  begin();
+
+
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
 var titleTransition = 0;
 var titleTransitionWhite = 100;
-var writing = 0
+var writing = 0;
+var buttonPressed = 5;
+var buttonX = 300;
+var buttonY = 100;
+var coordinateX = 600;
+var coordinateY = 440;
+var start = false;
+
 
 function titleScreen(){
+if (start == false) {
   writing = writing + 8;
   titleTransition=titleTransition+2;
   titleTransitionWhite= titleTransitionWhite+2;
@@ -27,5 +35,49 @@ function titleScreen(){
   textFont('monospace', 50);
   textSize(17);
   text('Mac Malainey, Hudson Shykowski, Richmond Naviza, Lora Ma', 618 , 278, 600, 50);
+}
+else if (start == true) {
+  fill('white');
 
+}
+}
+
+function triangles(){
+  //background triangles
+  fill(color(212, 225, 247, 180));
+  triangle(800,10 , 100, 625, 200, 200);
+  fill(color(127, 173, 249, 255));
+  triangle(30, 700, 580, 100, 860, 750);
+  fill(color(151, 188, 252, 180));
+  triangle(1100, 0, 1500, 0, 1000, 300);
+  fill(color(191, 209, 239, 150));
+  triangle(900, 100, 900, 800, 1500, 800);
+}
+
+function begin(){
+
+  if (mouseIsPressed && mouseX > 610 && mouseX < 910 && mouseY > 450 && mouseY < 550 && start == false) {
+
+    //begin button
+    for (i = 0; i < 255; i++){
+      buttonX = buttonX + 45;
+      buttonY = buttonY + 45;
+      coordinateX = coordinateX - 15;
+      coordinateY = coordinateY - 15;
+      buttonPressed++;
+      fill(color(162, 188, 229, titleTransition));
+      rect(coordinateX + 10, coordinateY + 10, buttonX, buttonY, buttonPressed, buttonPressed, buttonPressed, buttonPressed);
+      fill(color( 255, 255, 255, titleTransitionWhite));
+      rect(coordinateX, coordinateY, buttonX, buttonY, buttonPressed, buttonPressed, buttonPressed, buttonPressed);
+    }
+    start = true;
+  }
+
+  else {
+    fill(color(162, 188, 229, titleTransition));
+    rect(610, 450, 300, 100, 5, 5, 5, 5);
+    fill(color(255, 255, 255, titleTransitionWhite));
+    rect(610 - 10, 440, 300, 100, 5, 5, 5, 5);
+    start = false;
+  }
 }
